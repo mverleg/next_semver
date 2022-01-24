c0ffeef6f9b807cdda07a35dca47cd4c3e38c96e
Mark <mverleg.noreply@gmail.com>
2022-01-24 22:47:03 +0100

    Switch to Rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use ::rocket::routes;
+use ::rocket::Rocket;
+use ::rocket::Build;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build().mount("/", routes![hello])
 }
