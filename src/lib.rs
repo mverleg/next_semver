4e0e1c9988e1534b1c9befee0daeccc2c81d9027
Mark <mverleg.noreply@gmail.com>
2022-01-24 23:14:39 +0100

    Parse the part of semver number to bump

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+use semver::Version;
+
+#[derive(Clone, Copy)]
+pub enum Part {
+    Major,
+    Minor,
+    Patch,
+}
+
+pub fn bump(version: &Version, part: Part) -> Version {
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,48 @@
-use ::rocket::routes;
-use ::rocket::Rocket;
 use ::rocket::Build;
+use ::rocket::get;
+use ::rocket::launch;
+use ::rocket::request::FromParam;
+use ::rocket::Rocket;
+use ::rocket::routes;
+use ::semver::Version;
+
+use ::next_semver::Part;
+
+#[derive(Debug, Clone, Copy)]
+pub enum BumpPart {
+    Major,
+    Minor,
+    Patch,
+}
+
+impl<'a> FromParam<'a> for BumpPart {
+    type Error = ();
+
+    fn from_param(param: &'a str) -> Result<Self, Self::Error> {
+        Ok(match param {
+             "ma" | "major" | "breaking" => BumpPart::Major,
+             "mi" | "minor" | "feature" => BumpPart::Minor,
+             "pa" | "patch" | "fix" => BumpPart::Patch,
+            _ => return Err(()),
+        })
+    }
+}
+
+impl<'a> FromParam<'a> for BumpPart {
+    type Error = ();
+
+    fn from_param(param: &'a str) -> Result<Self, Self::Error> {
+        Ok(match param {
+             "ma" | "major" | "breaking" => BumpPart::Major,
+             "mi" | "minor" | "feature" => BumpPart::Minor,
+             "pa" | "patch" | "fix" => BumpPart::Patch,
+            _ => return Err(()),
+        })
+    }
+}
 
 #[get("/<part>/<version>")]
-fn hello(part: &str, version: &str) -> String {
+fn hello(part: BumpPart, version: &str) -> String {
     "Hello, world!".to_owned()
 }
 
