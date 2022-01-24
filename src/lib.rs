c0ffee8c3ad16dafe797dcbabdc4ce0b5dc7a30f
Mark <mverleg.noreply@gmail.com>
2022-01-24 23:56:22 +0100

    Implement bumping

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use semver::Version;
+use semver::{BuildMetadata, Version};
 
 #[derive(Clone, Copy)]
 pub enum Part {
@@ -8,5 +8,27 @@ pub enum Part {
 }
 
 pub fn bump(version: &Version, part: Part) -> Version {
-    unimplemented!()
+    match part {
+        Part::Major => Version {
+            major: version.major + 1,
+            minor: 0,
+            patch: 0,
+            pre: version.pre.clone(),
+            build: BuildMetadata::EMPTY,
+        },
+        Part::Minor => Version {
+            major: version.major,
+            minor: version.minor + 1,
+            patch: 0,
+            pre: version.pre.clone(),
+            build: BuildMetadata::EMPTY,
+        },
+        Part::Patch => Version {
+            major: version.major,
+            minor: version.minor,
+            patch: version.patch + 1,
+            pre: version.pre.clone(),
+            build: BuildMetadata::EMPTY,
+        },
+    }
 }
